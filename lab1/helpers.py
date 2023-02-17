1a41d716317e11bd63070b93fe5fecbde3c3009b
DenVilk <vvelikovich@gmail.com>
2023-02-17 14:04:14 +0300

    added calculator functionality

---
+++ lab1/helpers.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Operation(Enum):
+    ADD = 'add'
+    SUB = 'sub'
+    MULT = 'mult'
+    DIV = 'div'
--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,23 @@
 from constants import HELLO_WORLD
+from helpers import Operation
 
 def hello_world():
     print(HELLO_WORLD)
 
-def calculator():
-    pass
+def calculator(a: int, b: int, operation: Operation):
+    match operation:
+        case Operation.ADD:
+            print(a+b)
+        case Operation.SUB:
+            print(a-b)
+        case Operation.DIV:
+            try:
+                print(a/b)
+            except ZeroDivisionError:
+                print('[Error] Division by zero. Stopping.')
+        case Operation.MULT:
+            print(a*b)
+
 
 def create_array():
     pass
@@ -20,10 +33,17 @@ if __name__ == '__main__':
         case 1:
             hello_world()
         case 2:
-            calculator()
+            a, b = map(int, input('Enter number A and B (in one line):\n').split())
+            operation_input = input('Enter operation (add, sub, mult, div):\n')
+            try:
+                operation = Operation(operation_input)
+                calculator(a, b, operation)
+            except ValueError:
+                print('[Error] Invalid operation. Stopping')
+                
         case 3:
             create_array()
         case _:
             print("Invalid command. Stopping.")
-            
+
         
